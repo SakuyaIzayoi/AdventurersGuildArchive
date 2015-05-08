57d9e43f076c49c5eb8343efdb4044c412de029c
unknown <upyon@stevens.edu>
2015-05-08 15:07:22 -0400

    Added View Archive Menu for users

--- handleGuildmaster.cpp
+++ handleGuildmaster.cpp
@@ -4,6 +4,8 @@ void handleGuildmaster()
 {
     std::string input;
     int option, selection, end;
+    int option2;
+    bool end2 = true;
 
     std::cin.ignore();
 
@@ -20,7 +22,8 @@ void handleGuildmaster()
         std::cout << "1. Print guildmembers" << std::endl;
         std::cout << "2. Add member to guild" << std::endl;
         std::cout << "3. Remove member from guild" << std::endl;
-        std::cout << "4. Finish managing the guild" << std::endl <<std::endl;
+        std::cout << "4. View archive" << std::endl;
+        std::cout << "5. Finish managing the guild" << std::endl <<std::endl;
 
         std::cout << "Choose an option: ";
         getline(std::cin, input);
@@ -51,9 +54,55 @@ void handleGuildmaster()
                 }
                 guild.remove_member(adventurer_list[selection]);
                 break;
-            case 4:
+            case 5:
                 end++;
                 break;
+            case 4:
+                end2 = true;
+                while(end2){
+                    displayAdventurerListTable();
+                    std::cout << "Sort by:" << std::endl;
+                    std::cout << "1. name" << std::endl;
+                    std::cout << "2. hp" << std::endl;
+                    std::cout << "3. sp:" << std::endl;
+                    std::cout << "4. strength:" << std::endl;
+                    std::cout << "5. stamina:" << std::endl;
+                    std::cout << "6. magic" << std::endl;
+                    std::cout << "7. luck" << std::endl;
+                    std::cout << "8. Exit" << std::endl;
+
+                    std::cout << "Choose an option: ";
+                    getline(std::cin, input);
+                    std::stringstream(input) >> option2;
+
+                    switch(option2){
+                        case 1:
+                            sort("name", false);
+                            break;
+                        case 2:
+                            sort("hp", false);
+                            break;
+                        case 3:
+                            sort("sp", false);
+                            break;
+                        case 4:
+                            sort("str", false);
+                            break;
+                        case 5:
+                            sort("stam", false);
+                            break;
+                        case 6:
+                            sort("mag", false);
+                            break;
+                        case 7:
+                            sort("luk", false);
+                            break;
+                        case 8:
+                            end2 = false;
+                            break;
+                    }
+
+                }
             default:
                 break;
         }
